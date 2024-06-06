97c055da43f72303146268180cd4a1d846c6f947
Ahui-224 <3229503365@qq.com>
2024-06-06 14:59:12 +0800

    1
    1

--- web-server/db/db.js
+++ web-server/db/db.js
@@ -3,7 +3,7 @@ import config from '../src/config'
 
 const  conn = mysql.createConnection({
     host:"localhost", // 数据库的地址
-    user: "", // 账号
+    user: "root", // 账号
     password: "root", // 密码
     database: "web_shop", // 数据库名称
     multipleStatements: true,  // 允许多条sql同时查询
